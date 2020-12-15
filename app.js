00b334e5ffc9eaff5f22754507abba1d4583a8e1
PrashantRayamajhi <prashanrayamajhi@gmail.com>
2020-12-15 11:34:24 +0545

    modifiy and add code

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express= require('express');
-const app=express();
-app.use(express.static("public"));
-app.get('/', (_req,res)=>
-{
-    res.sendFile(__dirname+"/index.html");
+const express = require("express");
+const app = express();
+
+// importing routes
+const HomeRoute = require("./routes/home.route");
+
+// static files
+app.use(express.static(__dirname + "/public"));
+
+// setting up view engine
+app.set("view engine", "ejs");
+
+// setting up routes
+app.use("/", HomeRoute);
+
+// server
+app.listen(8080, () => {
+  console.log("Running on port 8080!");
 });
-
-
-app.listen(8080, ()=>
-{
-    console.log("Running on port 8080!");
-})
