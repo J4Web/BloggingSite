212b5d8124d7e0856457025aa6a01c0375faafbe
PrashantRayamajhi <prashanrayamajhi@gmail.com>
2020-12-16 13:42:25 +0545

    create blog form

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,7 +1,8 @@
 const route = require("express").Router();
 const controller = require("../controllers/home.controller");
 
-
 route.get("/", controller.getHome);
+
+route.get("/create", controller.getCreate);
 
 module.exports = route;
